85ce2399c9378e653a5198750446e704b5ba0ba3
HongGeun <ghdrms163@gmail.com>
2023-09-04 03:06:54 +0900

    [feat] 레디스 세션 기능 추가

--- e-commerce/src/main/java/hgk/ecommerce/ECommerceApplication.java
+++ e-commerce/src/main/java/hgk/ecommerce/ECommerceApplication.java
@@ -3,9 +3,11 @@ package hgk.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableRedisHttpSession
 public class ECommerceApplication {
 
     public static void main(String[] args) {
