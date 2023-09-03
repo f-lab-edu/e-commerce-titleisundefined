7f0dad8e3f1b1041c2163c6c042f4573cf0a143b
HongGeun <ghdrms163@gmail.com>
2023-09-04 03:01:16 +0900

    [feat] jpa & mysql 설정 기능 추가

---
+++ e-commerce/src/main/java/hgk/ecommerce/global/config/DataSourceConfiguration.java
@@ -0,0 +1,74 @@
+package hgk.ecommerce.global.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import javax.sql.DataSource;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration
+public class DataSourceConfiguration {
+    private static final String MASTER_DATASOURCE = "masterDataSource";
+    private static final String SLAVE_DATASOURCE = "slaveDataSource";
+
+    @Bean(MASTER_DATASOURCE)
+    @ConfigurationProperties(prefix = "spring.datasource.master.hikari")
+    public DataSource masterDataSource() {
+        return DataSourceBuilder.create()
+                // HikariDataSource 타입의 DataSource 객체를 생성한다.
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean(SLAVE_DATASOURCE)
+    @ConfigurationProperties(prefix = "spring.datasource.slave.hikari")
+    public DataSource slaveDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    public DataSource routingDataSource(
+            @Qualifier(MASTER_DATASOURCE) DataSource masterDataSource,
+            @Qualifier(SLAVE_DATASOURCE) DataSource slaveDataSource) {
+
+        RoutingDataSource routingDataSource = new RoutingDataSource();
+        HashMap<Object, Object> sourceHash = new HashMap<>();
+        sourceHash.put("master", masterDataSource);
+        sourceHash.put("slave", slaveDataSource);
+
+        Map<Object, Object> datasourceMap =
+                Collections.unmodifiableMap(new LinkedHashMap<>(sourceHash));
+        routingDataSource.setTargetDataSources(datasourceMap);
+        routingDataSource.setDefaultTargetDataSource(masterDataSource);
+
+        return routingDataSource;
+    }
+
+    @Primary
+    @Bean
+    public DataSource dataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+
+    static class RoutingDataSource extends AbstractRoutingDataSource {
+        @Override
+        protected Object determineCurrentLookupKey() {
+            boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+
+            return isReadOnly ? "slave" : "master";
+        }
+    }
+}
