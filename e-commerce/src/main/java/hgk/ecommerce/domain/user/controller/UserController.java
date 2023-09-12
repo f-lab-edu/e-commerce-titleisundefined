85e300b87d0f91c4df59c8cd0c158c895b528db3
HongGeun <ghdrms163@gmail.com>
2023-09-13 00:03:19 +0900

    [modify] 오타 수정

--- e-commerce/src/main/java/hgk/ecommerce/domain/user/controller/UserController.java
+++ e-commerce/src/main/java/hgk/ecommerce/domain/user/controller/UserController.java
@@ -5,11 +5,20 @@ import hgk.ecommerce.domain.user.User;
 import hgk.ecommerce.domain.user.dto.request.UserSign;
 import hgk.ecommerce.domain.user.dto.request.UserLogin;
 import hgk.ecommerce.domain.user.service.UserService;
+import hgk.ecommerce.global.storage.NaverObjectStorage;
+import hgk.ecommerce.global.storage.UploadFile;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 import static hgk.ecommerce.domain.common.dto.MyResponse.*;
 
@@ -37,9 +46,10 @@ public class UserController {
         return RESPONSE_OK;
     }
 
-    @DeleteMapping("sign-out")
+    @DeleteMapping("/sign-out")
     public ResponseEntity signOut(@AuthCheck User user) {
         userService.delete(user);
         return RESPONSE_OK;
     }
 }
+
